90a90d8fe83d40f4f101678fdcba585e2d8994ed
jj <jj@r2src.com>
2012-01-10 17:12:38 +0100

    got yaml parsing to work

--- core/core.py
+++ core/core.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
 from werkzeug.routing import Map, Rule
+import yaml
 
 # populate map
 rules = []
 stream = file('manifest.yaml', 'r')
-dict = yaml.load(stream)
-for uri in dict['uris']:
-  rules.append(Rule(uri['path'], uri['name']))
+uris = yaml.load(stream)['uris']
+for name in uris:
+  rules.append(Rule(uris[name]['path'], name))
 map = Map(rules)
+# it works!
+exit()
 
 def dispatch(request):
   map.bind_to_environ(request.environ)
   endpoint, values = adapter.match()
   try:
     eval("%s_%s" % (request.method.lower(), endpoint))
+  except:
+    pass
 
 def application(environ, start_response):
   request = Request(environ)
